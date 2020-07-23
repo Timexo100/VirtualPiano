415eb88b9deefaf12636d7cd76497bbffb9b3172
Tim <tmuhin@gmail.com>
2020-07-23 19:13:17 +0300

    Init commit.

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("keypress", function(e) {
+  console.log(e.code);
+  if (e.code === "KeyA") {
+    console.log("A pressed");
+  }
+  if (e.code === "KeyS") {
+    console.log("S pressed");
+  }
+  if (e.code === "KeyD") {
+    console.log("D pressed");
+  }
+  if (e.code === "KeyF") {
+    console.log("F pressed");
+  }
+  if (e.code === "KeyG") {
+    console.log("G pressed");
+  }
+  if (e.code === "KeyH") {
+    console.log("H pressed");
+  }
+  if (e.code === "KeyJ") {
+    console.log("J pressed");
+  }
+  if (e.code === "KeyW") {
+    console.log("W pressed");
+  }
+  if (e.code === "KeyE") {
+    console.log("E pressed");
+  }
+  if (e.code === "KeyT") {
+    console.log("T pressed");
+  }
+  if (e.code === "KeyY") {
+    console.log("Y pressed");
+  }
+  if (e.code === "KeyU") {
+    console.log("U pressed");
+  }
+});
